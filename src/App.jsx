10483cb7433cb42c0783d1d197b107e5124f20c5
Muhib Ullah <muhibullah0603@gmail.com>
2026-08-01 15:48:30 +0500

    integrated tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind is working!
+      </h1>
+    </div>
   )
 }
 
